3cf27c70c19bdf12ac6a1832cde28b104d8de6fa
WangSoooCute <38801071+WangSoooCute@users.noreply.github.com>
2022-09-24 09:58:22 +0800

    Update and rename P19.1.cpp to P18.1_18.2.cpp

--- 线性表/顺序实现大题/P18.1_18.2.cpp
+++ 线性表/顺序实现大题/P18.1_18.2.cpp
@@ -1,4 +1,12 @@
-﻿#include <cstdio>
+/*
+P18.1
+从顺序表中删除具有最小值的元素(假设唯一)并由函数返回被删元素的值。
+空出的位置由最后一个元素填补, 若顺序表为空则显示出错信息并退出运行
+P18.2
+设计一个高效算法，将顺序表L的所有元素逆置，要求算法的空间复杂度为O(1)
+*/
+
+#include <cstdio>
 #include <iostream>
 #include <algorithm>
 
@@ -42,10 +50,6 @@ void PrintList(List list) {
 
 #pragma endregion
 
-//P19.1
-//从顺序表中删除具有最小值的元素(假设唯一)并由函数返回被删元素的值。空出的位
-//置由最后一个元素填补, 若顺序表为空则显示出错信息并退出运行
-
 int FindMin(List& list)
 {
 	if (!list.length) {
@@ -53,9 +57,9 @@ int FindMin(List& list)
 		return -1;
 	}
 
-	int Min = 9999;
+	int Min = list.data[0];
 	int point = 0;
-	for (int i = 0; i < list.length; i++)
+	for (int i = 1; i < list.length; i++)
 	{
 		if (list.data[i] < Min)
 		{
