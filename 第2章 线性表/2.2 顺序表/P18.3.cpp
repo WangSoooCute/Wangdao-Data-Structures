4bd11184f6248e8be162d6191e45f0c295e6ef8c
WangSoooCute <38801071+WangSoooCute@users.noreply.github.com>
2022-09-30 21:25:16 +0800

    Update P18.3.cpp

--- 第2章 线性表/2.2 顺序表/P18.3.cpp
+++ 第2章 线性表/2.2 顺序表/P18.3.cpp
@@ -46,6 +46,7 @@ void PrintList(List list) {
 //对长度为n的顺序表L, 编写一个时间复杂度为O(m)空间复杂度为O(1)的算法,
 //该算法删除线性表中所有值为x的数据元素
 
+//方法1:用k记录顺序表L中等于x的元素个数，边扫描L边统计k，并将不等于x的元素前移k个位置。扫描结束后修改L的长度。
 void DeleteX(List& list, int X) {
 	int k = 0, i = 0;
 	while (i < list.length) {
@@ -57,6 +58,18 @@ void DeleteX(List& list, int X) {
 	}
 	list.length = list.length - k;
 }
+
+//方法2：用k记录顺序表L中不等于x的元素个数（即需要保存的元素个数），扫描时将不等于x的元素移动到下标k的位置，并更新k值。扫描结束后修改L的长度。
+void DeleteX(List& list, int X)｛
+int k=0，i；
+for （i=0；i<L.length；i++）
+if（L.data［i］！=x）｛
+L.data［k］=L.data［i］；
+k++；
+｝
+L.length=k；
+
+//方法3:L不是有序表的话还可把倒数的几个元素换到前面
 
 int main()
 {
